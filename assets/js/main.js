4723146737d9632b16e04efcd8e945fab4993a34
adibmansuri511 <adibmansuri.developer@gmail.com>
2024-01-02 17:49:25 +0530

    Update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,22 @@ function reverseString() {
     // Take input from the user
     const str = (document.getElementById("str").value).toString();
 
-    // empty string
-    let reverseStr = "";
+    // Step 1. Use the split() method to return a new array
+    const splitString = str.split("");      // const splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
 
-    for (let i = str.length - 1; i >= 0; i--) {
-        reverseStr += str[i];
-    }
 
-    let newStr = `Reversed String : ${reverseStr}`;
+    // Step 2. Use the reverse() method to reverse the new created array
+    const reverseArray = splitString.reverse();     // const reverseArray = ["h", "e", "l", "l", "o"];
+    // ["o", "l", "l", "e", "h"]
 
-    console.log(newStr);
-    document.querySelector('#h4').innerHTML = newStr;
+
+    // Step 3. Use the join() method to join all elements of the array into a string
+    const joinArray = reverseArray.join("");     // const joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+
+    let reverseStr = `Reversed String : ${joinArray}`;
+
+    console.log(reverseStr);
+    document.querySelector('#h4').innerHTML = reverseStr;
 }
